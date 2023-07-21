3961478c78dc382b86f9a2b9caf55cbed0e29d35
Sourabh-Bharale <sourabhbharale80@gmail.com>
2023-07-21 20:12:13 +0530

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+
+export const metadata = {
+  title: 'suc-it',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  )
+}

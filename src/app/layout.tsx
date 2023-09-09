46ee6098477426a2f8b5c508d7c2bb40f3516c33
Sourabh-Bharale <sourabhbharale80@gmail.com>
2023-09-09 14:13:00 +0530

    robots metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,22 @@ import { Inter } from 'next/font/google'
 
 export const metadata = {
   title: 'Hive',
-  description: 'Welcome to Hive - Your Personalized Community Platform! Create your own HiveHub, connect with like-minded individuals, and share your passions. Join today to post, interact, and discover a vibrant world of diverse content. Unleash your creativity and be part of a buzzing hive of inspiration and engagement. Sign up now and experience the power of community-driven connections.'
+  description: 'Welcome to Hive - Your Personalized Community Platform! Create your own HiveHub, connect with like-minded individuals, and share your passions. Join today to post, interact, and discover a vibrant world of diverse content. Unleash your creativity and be part of a buzzing hive of inspiration and engagement. Sign up now and experience the power of community-driven connections.',
+  verification: {
+    google: 'nAV5D-dLP4jkCuMD1eCj6BX1zzXwTFqmkNcFPXng9AA'
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 const inter = Inter({
